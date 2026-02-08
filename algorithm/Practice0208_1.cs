48bece32606fb92f12edf7a06a6e838eb1718185
Psofcake <g.psofcake@gmail.com>
2026-02-08 23:44:00 +0900

    260208 연습문제
    문자열 뒤집기, 최대 숫자 찾기

---
+++ algorithm/Practice0208_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm
+{
+    internal class Practice0208_1
+    {
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine("Input string");
+            string input = Console.ReadLine();  //문자열 입력하기
+            string output = ReverseStr(input);  //주어진 문자열 뒤집기
+            Console.WriteLine(output);  //출력
+        }*/
+
+        static string ReverseStr(string str)
+        {
+            char[] CharArray = str.ToCharArray(); //문자 배열로 변환
+            Array.Reverse(CharArray); //배열 뒤집기
+            return new string(CharArray); //다시 문자열로 변환
+        }
+    }
+}
---
+++ algorithm/Practice0208_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm
+{
+    internal class Practice0208_2
+    {
+        /*static void Main(string[] args)
+        {
+            int[] numbers = { 1,3,5,7,9 };
+            int max = FindMaximum(numbers); //정수 배열에서 최대 숫자 찾기
+            Console.WriteLine(max);
+        }*/
+        static int FindMaximum(int[] numbers) 
+        {
+            int max = numbers[0];
+            
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if(numbers[i]>max) max = numbers[i];
+            }
+
+            //foreach (int number in numbers) { if (number > max) max = number; } 위와 동일
+            return max;
+        }
+    }
+}
